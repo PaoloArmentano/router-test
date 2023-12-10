a1fa423a88f8880391d29c59852cf2a012a944de
Paolo Armentano <113731161+PaoloArmentano@users.noreply.github.com>
2023-12-10 22:53:56 +0000

    yo

--- src/components/About.js
+++ src/components/About.js
@@ -2,6 +2,7 @@ export default function About(){
     return (
         <div className="about">
             <h2>Welcome to the About page</h2>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nobis distinctio nostrum eius eligendi, asperiores omnis non similique ea voluptatibus earum velit laboriosam, maxime excepturi impedit cumque aspernatur ipsa ab esse unde! Officia, similique voluptatum laboriosam culpa ducimus et. Iste, suscipit! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nobis distinctio nostrum eius eligendi, asperiores omnis non similique ea voluptatibus earum velit laboriosam, maxime excepturi impedit cumque aspernatur ipsa ab esse unde! Officia, similique voluptatum laboriosam culpa ducimus et. Iste, suscipit!</p>
         </div>
     )
 }
